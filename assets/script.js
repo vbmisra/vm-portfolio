dd98623e0e59c01fb4bce56c5523258b2c233cb4
Invincible <87626336+vbmisra@users.noreply.github.com>
2021-11-08 16:46:56 -0800

    committing changes to html files

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,48 @@
-let aboutMeBtn = document.querySelector('#about-me');
-let projectsBtn = document.querySelector('#projects');
-let contactBtn = document.querySelector('#contact-me');
-let resumeBtn = document.querySelector('#resume');
+// let aboutMeBtn = document.querySelector('#about-me');
+// let projectsBtn = document.querySelector('#projects');
+// let contactBtn = document.querySelector('#contact-me');
+let resumeBtn = document.querySelector('#resume-btn');
+let resumeBox = document.querySelector('#resume-text-box');
+let recentText = document.querySelector('#recent-text');
+let timerEl = document.querySelector('#timer-element');
 
-const workHistory=[
-    "Spent 4 years in operations within the biotech industry",
-    "Worked 2 years in production",
-    "Worked 1 year in quality",
-    "Worked 1 year in process sustaining"
-]
+const statement = "I'm bored."
+let timer = 30
 
-const languages=[
-    "Versed in the following languages",
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "jQuery",
-    "Python",
-    "R",
-    "Frameworks:",
-    "Bootstrap",
-    
-]
+function startTimer() {
+    let timerInterval = setInterval(function () {
+        timer--
+        timerEl.textContent = timer
+        if(timerInterval == 0) {
+            clearInterval(timer)
+        }
+    }, 1000)
+}
 
+// const workHistory=[
+//     "Spent 4 years in operations within the biotech industry",
+//     "Worked 2 years in production",
+//     "Worked 1 year in quality",
+//     "Worked 1 year in process sustaining"
+// ]
+
+// const languages=[
+//     "Versed in the following languages",
+//     "HTML",
+//     "CSS",
+//     "JavaScript",
+//     "jQuery",
+//     "Python",
+//     "R",
+//     "Frameworks:",
+//     "Bootstrap",
 //projectsBtn.addEventListener('click', changeLocation);
 
-projectsBtn.addEventListener('click', toProjects);
+// projectsBtn.addEventListener('click', toProjects);
 
-resumeBtn.addEventListener('click', toResume);
 
 function toProjects() {
     location.href='./projects.html'
-}
-
-function toResume() {
-    location.href='./resume.html'
 }
 
 
